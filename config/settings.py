81ea481ea79dbe612a2d76ad422374f5172d7ee6
ADONISJSG <adonissol2006@gmail.com>
2026-07-16 00:36:49 -0500

    fix: mostrar errores de Django en Render

--- config/settings.py
+++ config/settings.py
@@ -192,3 +192,27 @@ CSRF_COOKIE_SECURE = not DEBUG
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
